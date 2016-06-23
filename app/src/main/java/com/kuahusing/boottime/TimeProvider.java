03f59642d486ece522303bebaef2761d23ece8a2
kuahusing <foreverhgx@gmail.com>
2016-06-23 13:39:34 +0800

    remove useless code

--- app/src/main/java/com/kuahusing/boottime/TimeProvider.java
+++ app/src/main/java/com/kuahusing/boottime/TimeProvider.java
@@ -8,14 +8,23 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+enum Time {
+    Day, Hour, Minutes
+}
 
 /**
  * Created by kuahusing on 16/6/22.
  */
 public class TimeProvider extends AppWidgetProvider {
-
+    private static int i = 0;
+    private static List<String> stringArray = new ArrayList<>();
+    Context mContext;
 
     private String getTime(long time, Time type) {
 //        long week = time / (1000 * 60 * 60 * 24 * 7);
@@ -40,9 +49,28 @@ public class TimeProvider extends AppWidgetProvider {
     }
 
     @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        mContext = context;
+        if (stringArray.size() == 0) {
+            getString();
+        }
+        Toast.makeText(context, stringArray.get(i), Toast.LENGTH_SHORT).show();
+        i++;
+        if (i > 28) {
+            i = 0;
+        }
+
+    }
+
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+//        mContextext = context;
         long time = SystemClock.elapsedRealtime();
+
+
         for (int id :
                 appWidgetIds) {
             RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.excited);
@@ -50,8 +78,9 @@ public class TimeProvider extends AppWidgetProvider {
             rv.setTextViewText(R.id.hour, getTime(time, Time.Hour));
             rv.setTextViewText(R.id.minute, getTime(time, Time.Minutes));
 
-            Intent i = new Intent(context, SimpleService.class);
-            PendingIntent pendingIntent = PendingIntent.getService(context, 0, i, 0);
+            Intent i = new Intent(context, TimeProvider.class);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, i, 0);
             rv.setOnClickPendingIntent(R.id.main, pendingIntent);
 
             rv.setTextViewText(R.id.emoji, getRandomEmoji());
@@ -72,8 +101,18 @@ public class TimeProvider extends AppWidgetProvider {
         return new String(Character.toChars(emojis[new Random().nextInt(emojis.length - 1)]));
 
     }
-}
 
-enum Time {
-    Day, Hour, Minutes
+
+    private void getString() {
+        for (int i = 0; i < 29; i++) {
+            String s = "warn";
+            s += i;
+            int resourceId = mContext.getResources().getIdentifier(s, "string", mContext.getPackageName());
+            if (resourceId > 0) {
+                stringArray.add(mContext.getString(resourceId) + getRandomEmoji());
+            }
+
+        }
+
+    }
 }
